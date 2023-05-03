9525572a93d194ba703a95cb192a2f934c7fef65
а б <vnmiha1@mail.ru>
2023-05-03 15:33:23 +0300

    Добавьте файлы проекта.

---
+++ TeleCompany/Pages/Auth.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace TeleCompany.Pages
+{
+    public partial class Auth : Page
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Registration());
+        }
+    }
+}
